e8065241adbc4138c365ce504deab8182dc12b3d
Dmytro Holub <dmitriy.golub@gmail.com>
2023-09-25 12:55:34 +0200

    add sub account api keys

--- backend/app/app/services/okx_core/lib/Broker_api.py
+++ backend/app/app/services/okx_core/lib/Broker_api.py
@@ -90,9 +90,10 @@ class BrokerAPI(Client):
             "ip": ip,
             "perm": perm,
         }
+        print("Params", params)
         result = self._request_with_params(POST, ND_CREAET_APIKEY, params)
-        print(result)
-        print(params)
+        print("Result", result)
+
         return result
 
     def nd_select_apikey(self, subAcct="", apiKey=""):
