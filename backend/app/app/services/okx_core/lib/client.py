acde8c7a6af12392650eb172670bfb39fe75eb40
Dmytro Holub <dmitriy.golub@gmail.com>
2023-09-25 15:10:47 +0200

    add sub account api keys

--- backend/app/app/services/okx_core/lib/client.py
+++ backend/app/app/services/okx_core/lib/client.py
@@ -42,7 +42,7 @@ class Client(object):
         if method == c.GET:
             response = requests.get(url, headers=header)
         elif method == c.POST:
-            response = requests.zost(url, data=body, headers=header)
+            response = requests.post(url, data=body, headers=header)
 
         # exception handle
         # print(response.headers)
