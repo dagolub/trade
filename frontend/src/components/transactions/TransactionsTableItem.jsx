5135a3c1866cdba24933ef16d888fa448874081f
Dmytro Holub <dmitriy.golub@gmail.com>
2023-09-28 08:49:01 +0200

    change front

--- frontend/src/components/transactions/TransactionsTableItem.jsx
+++ frontend/src/components/transactions/TransactionsTableItem.jsx
@@ -21,28 +21,35 @@ function TransactionTableItem(props) {
             </td>
 
             <td className={"px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap td" + props.id}>
-                <div className="font-medium text-slate-800 dark:text-slate-100">{props.from_wallet.length > 12 ? props.from_wallet.substring(0, 5) + " ... " + props.from_wallet.substring(props.from_wallet.length - 5) : props.from_wallet}</div>
+                <div
+                    className="font-medium text-slate-800 dark:text-slate-100">{props.from_wallet.length > 12 ? props.from_wallet.substring(0, 5) + " ... " + props.from_wallet.substring(props.from_wallet.length - 5) : props.from_wallet}</div>
             </td>
             <td className={"px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap td" + props.id}>
-                <div className="font-medium text-slate-800 dark:text-slate-100">{props.to_wallet.length > 12 ? props.to_wallet.substring(0, 5) + " ... " + props.to_wallet.substring(props.to_wallet.length - 5) : props.to_wallet}</div>
+                <div
+                    className="font-medium text-slate-800 dark:text-slate-100">{props.to_wallet.length > 12 ? props.to_wallet.substring(0, 5) + " ... " + props.to_wallet.substring(props.to_wallet.length - 5) : props.to_wallet}</div>
 
             </td>
             <td className={"px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap td" + props.id}>
-                { props.tx.length > 12 && <div
+                <div
                     className="font-medium text-slate-800 dark:text-slate-100">{props.tx.length > 9 ? props.tx.substring(0, 5) + " ... " + props.tx.substring(props.tx.length - 5) : props.tx}
-                </div>}
-                { props.tx.length > 12 &&
-                <svg fill="#64748b" style={{"display": "inline-block", "margin-left": "5px", "cursor": "pointer"}} height="20px" width="20px" viewBox="0 0 330 330" onClick={()=>handlerCopy(props.tx)}>
+                    {props.tx.length > 12 &&
+                        <svg fill="#64748b"
+                             style={{"display": "inline-block", "margin-left": "5px", "cursor": "pointer"}}
+                             height="20px" width="20px" viewBox="0 0 330 330" onClick={() => handlerCopy(props.tx)}>
                             <g>
                                 <path d="M35,270h45v45c0,8.284,6.716,15,15,15h200c8.284,0,15-6.716,15-15V75c0-8.284-6.716-15-15-15h-45V15
                                     c0-8.284-6.716-15-15-15H35c-8.284,0-15,6.716-15,15v240C20,263.284,26.716,270,35,270z M280,300H110V90h170V300z M50,30h170v30H95
                                     c-8.284,0-15,6.716-15,15v165H50V30z"/>
-                                <path d="M155,120c-8.284,0-15,6.716-15,15s6.716,15,15,15h80c8.284,0,15-6.716,15-15s-6.716-15-15-15H155z"/>
-                                <path d="M235,180h-80c-8.284,0-15,6.716-15,15s6.716,15,15,15h80c8.284,0,15-6.716,15-15S243.284,180,235,180z"/>
+                                <path
+                                    d="M155,120c-8.284,0-15,6.716-15,15s6.716,15,15,15h80c8.284,0,15-6.716,15-15s-6.716-15-15-15H155z"/>
+                                <path
+                                    d="M235,180h-80c-8.284,0-15,6.716-15,15s6.716,15,15,15h80c8.284,0,15-6.716,15-15S243.284,180,235,180z"/>
                                 <path d="M235,240h-80c-8.284,0-15,6.716-15,15c0,8.284,6.716,15,15,15h80c8.284,0,15-6.716,15-15C250,246.716,243.284,240,235,240z
                                     "/>
                             </g>
-                </svg>}
+                        </svg>}
+                </div>
+
             </td>
             <td className={"px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap td" + props.id}>
                 <div className="font-medium text-slate-800 dark:text-slate-100">{props.amount}</div>
@@ -59,22 +66,22 @@ function TransactionTableItem(props) {
             </td>
             <td className={"px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap w-px td" + props.id}>
                 <div className="space-x-1" style={{"display": "flex"}}>
-                        <button onClick={() => viewHandler(props.id)}
+                    <button onClick={() => viewHandler(props.id)}
                             className="text-slate-400 hover:text-slate-500 dark:text-slate-500 dark:hover:text-slate-400 rounded-full">
-                            <svg fill="#64748b" height="20px" width="20px" viewBox="0 0 80.794 80.794">
+                        <svg fill="#64748b" height="20px" width="20px" viewBox="0 0 80.794 80.794">
+                            <g>
                                 <g>
-                                    <g>
-                                        <path d="M79.351,38.549c-0.706-0.903-17.529-22.119-38.953-22.119c-21.426,0-38.249,21.216-38.955,22.119L0,40.396l1.443,1.847
+                                    <path d="M79.351,38.549c-0.706-0.903-17.529-22.119-38.953-22.119c-21.426,0-38.249,21.216-38.955,22.119L0,40.396l1.443,1.847
                                         c0.706,0.903,17.529,22.12,38.955,22.12c21.424,0,38.247-21.217,38.953-22.12l1.443-1.847L79.351,38.549z M40.398,58.364
                                         c-15.068,0-28.22-13.046-32.643-17.967c4.425-4.922,17.576-17.966,32.643-17.966c15.066,0,28.218,13.045,32.642,17.966
                                         C68.614,45.319,55.463,58.364,40.398,58.364z"/>
-                                        <path d="M40.397,23.983c-9.052,0-16.416,7.363-16.416,16.414c0,9.053,7.364,16.417,16.416,16.417s16.416-7.364,16.416-16.417
+                                    <path d="M40.397,23.983c-9.052,0-16.416,7.363-16.416,16.414c0,9.053,7.364,16.417,16.416,16.417s16.416-7.364,16.416-16.417
                                         C56.813,31.346,49.449,23.983,40.397,23.983z M40.397,50.813c-5.744,0-10.416-4.673-10.416-10.417
                                         c0-5.742,4.672-10.414,10.416-10.414c5.743,0,10.416,4.672,10.416,10.414C50.813,46.14,46.14,50.813,40.397,50.813z"/>
-                                    </g>
                                 </g>
-                            </svg>
-                        </button>
+                            </g>
+                        </svg>
+                    </button>
                 </div>
             </td>
         </tr>
