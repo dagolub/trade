5f77356d552f191151c373a858cbe968f243ce85
Dmytro Holub <dmitriy.golub@gmail.com>
2023-09-28 08:51:18 +0200

    change front

--- frontend/src/components/transactions/TransactionsTableItem.jsx
+++ frontend/src/components/transactions/TransactionsTableItem.jsx
@@ -22,11 +22,45 @@ function TransactionTableItem(props) {
 
             <td className={"px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap td" + props.id}>
                 <div
-                    className="font-medium text-slate-800 dark:text-slate-100">{props.from_wallet.length > 12 ? props.from_wallet.substring(0, 5) + " ... " + props.from_wallet.substring(props.from_wallet.length - 5) : props.from_wallet}</div>
+                    className="font-medium text-slate-800 dark:text-slate-100">{props.from_wallet.length > 12 ? props.from_wallet.substring(0, 5) + " ... " + props.from_wallet.substring(props.from_wallet.length - 5) : props.from_wallet}
+                    {props.from_wallet.length > 12 &&
+                        <svg fill="#64748b"
+                             style={{"display": "inline-block", "margin-left": "5px", "cursor": "pointer"}}
+                             height="20px" width="20px" viewBox="0 0 330 330" onClick={() => handlerCopy(props.from_wallet)}>
+                            <g>
+                                <path d="M35,270h45v45c0,8.284,6.716,15,15,15h200c8.284,0,15-6.716,15-15V75c0-8.284-6.716-15-15-15h-45V15
+                                    c0-8.284-6.716-15-15-15H35c-8.284,0-15,6.716-15,15v240C20,263.284,26.716,270,35,270z M280,300H110V90h170V300z M50,30h170v30H95
+                                    c-8.284,0-15,6.716-15,15v165H50V30z"/>
+                                <path
+                                    d="M155,120c-8.284,0-15,6.716-15,15s6.716,15,15,15h80c8.284,0,15-6.716,15-15s-6.716-15-15-15H155z"/>
+                                <path
+                                    d="M235,180h-80c-8.284,0-15,6.716-15,15s6.716,15,15,15h80c8.284,0,15-6.716,15-15S243.284,180,235,180z"/>
+                                <path d="M235,240h-80c-8.284,0-15,6.716-15,15c0,8.284,6.716,15,15,15h80c8.284,0,15-6.716,15-15C250,246.716,243.284,240,235,240z
+                                    "/>
+                            </g>
+                        </svg>}
+                </div>
             </td>
             <td className={"px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap td" + props.id}>
                 <div
-                    className="font-medium text-slate-800 dark:text-slate-100">{props.to_wallet.length > 12 ? props.to_wallet.substring(0, 5) + " ... " + props.to_wallet.substring(props.to_wallet.length - 5) : props.to_wallet}</div>
+                    className="font-medium text-slate-800 dark:text-slate-100">{props.to_wallet.length > 12 ? props.to_wallet.substring(0, 5) + " ... " + props.to_wallet.substring(props.to_wallet.length - 5) : props.to_wallet}
+                    {props.to_wallet.length > 12 &&
+                        <svg fill="#64748b"
+                             style={{"display": "inline-block", "margin-left": "5px", "cursor": "pointer"}}
+                             height="20px" width="20px" viewBox="0 0 330 330" onClick={() => handlerCopy(props.to_wallet)}>
+                            <g>
+                                <path d="M35,270h45v45c0,8.284,6.716,15,15,15h200c8.284,0,15-6.716,15-15V75c0-8.284-6.716-15-15-15h-45V15
+                                    c0-8.284-6.716-15-15-15H35c-8.284,0-15,6.716-15,15v240C20,263.284,26.716,270,35,270z M280,300H110V90h170V300z M50,30h170v30H95
+                                    c-8.284,0-15,6.716-15,15v165H50V30z"/>
+                                <path
+                                    d="M155,120c-8.284,0-15,6.716-15,15s6.716,15,15,15h80c8.284,0,15-6.716,15-15s-6.716-15-15-15H155z"/>
+                                <path
+                                    d="M235,180h-80c-8.284,0-15,6.716-15,15s6.716,15,15,15h80c8.284,0,15-6.716,15-15S243.284,180,235,180z"/>
+                                <path d="M235,240h-80c-8.284,0-15,6.716-15,15c0,8.284,6.716,15,15,15h80c8.284,0,15-6.716,15-15C250,246.716,243.284,240,235,240z
+                                    "/>
+                            </g>
+                        </svg>}
+                </div>
 
             </td>
             <td className={"px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap td" + props.id}>
